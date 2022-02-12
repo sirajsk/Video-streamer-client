3ddc101dc9f0a0f3cce45d4bbbc7a316e10073d0
sirajsk <sirajkpd12@gmail.com>
2022-02-12 08:01:58 +0530

    video upload completed

--- src/components/UploadVideo/UploadVideo.js
+++ src/components/UploadVideo/UploadVideo.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import Backdrop from '@mui/material/Backdrop';
 import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Banner from '../Assets/V.svg';
 import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
 import Skeleton from '@mui/material/Skeleton';
-
+import axios from 'axios';
+import '../UploadVideo/UploadVideo.scss'
 
 var cardStyle = {
     display: 'block',
@@ -32,11 +32,109 @@ const style = {
     boxShadow: 24,
     p: 4,
 };
+const chunkSize = 5 * 1024;
 
 export default function UploadVideo() {
-    const [open, setOpen] = React.useState(false);
+    const [dropZoneActive,setDropZoneActive]=useState(false)
+    const [files,setFiles]=useState([])
+    const [currentFileIndex,setCurrentFileIndex]=useState(null)
+    const [lastUploadedFileIndex, setLastUploadedFileIndex] = useState(null);
+    const [currentChunkIndex, setCurrentChunkIndex] = useState(null);
+    const [open, setOpen] =useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+    function handleDrop(e){
+        e.preventDefault()
+        setFiles([...files,...e.dataTransfer.files])
+
+    }
+
+    function readAndUploadCurrentChunk(){
+        const reader=new FileReader();
+        const file=files[currentFileIndex]
+        if(!file){
+            return;
+
+        }
+        const from = currentChunkIndex * chunkSize;
+        const to = from + chunkSize;
+        const blob = file.slice(from, to);
+        reader.onload = e => uploadChunk(e);
+        reader.readAsDataURL(blob);
+    }
+
+    function uploadChunk(readerEvent){
+        const file = files[currentFileIndex];
+        const data = readerEvent.target.result;
+        const params = new URLSearchParams();
+
+        params.set('name', file.name);
+        params.set('size', file.size);
+        params.set('currentChunkIndex', currentChunkIndex);
+        params.set('totalChunks', Math.ceil(file.size / chunkSize)); 
+        const headers = { 'Content-Type': 'application/octet-stream' };
+        const url = 'http://localhost:3001/api/upload?' + params.toString();
+
+        axios.post(url,data,{headers}).then(response=>{
+            console.log(response)
+            const file = files[currentFileIndex];
+            const filesize = files[currentFileIndex].size;
+            const chunks = Math.ceil(filesize / chunkSize) - 1;
+            const isLastChunk = currentChunkIndex === chunks;
+
+            if (isLastChunk) {
+                file.finalFilename = response.data.finalFilename;
+                setLastUploadedFileIndex(currentFileIndex);
+                setCurrentChunkIndex(null);
+                console.log('is last chunk')
+                setDropZoneActive(false)
+                
+            }
+            else {
+                setCurrentChunkIndex(currentChunkIndex + 1);
+            }
+        })
+    }
+
+
+
+    useEffect(() => {
+        if (lastUploadedFileIndex === null) {
+            return;
+        }
+        const isLastFile = lastUploadedFileIndex === files.length - 1;
+        const nextFileIndex = isLastFile ? null : currentFileIndex + 1;
+        setCurrentFileIndex(nextFileIndex);
+    }, [lastUploadedFileIndex]);
+
+
+
+
+    useEffect(() => {
+    if(files.length>0){
+        if(currentFileIndex ===null){
+            setCurrentFileIndex(
+                lastUploadedFileIndex === null ? 0 : lastUploadedFileIndex + 1
+            );
+        }
+    }
+    }, [files.length])
+
+
+    useEffect(() => {
+        if (currentFileIndex !== null) {
+            setCurrentChunkIndex(0);
+        }
+    }, [currentFileIndex]);
+
+
+   useEffect(() => {
+   if(currentChunkIndex !==null){
+    readAndUploadCurrentChunk()
+   } 
+   }, [currentChunkIndex])
+   
+
     return (
         <>
 
@@ -59,7 +157,7 @@ export default function UploadVideo() {
                         <Button variant="contained" component="span" onClick={handleOpen}>
                             Upload video
                         </Button>
-                       
+
                         <Modal
                             aria-labelledby="transition-modal-title"
                             aria-describedby="transition-modal-description"
@@ -75,13 +173,20 @@ export default function UploadVideo() {
                                 <Box sx={style}>
                                     <Typography id="transition-modal-title" variant="h6" component="h2">
                                         UPLOAD VIDEO
-                                    </Typography>
+                                    </Typography>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
                                     <hr />
-                                    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                                    <div
+                                        onDragOver={e=>{setDropZoneActive(true);e.preventDefault()}}
+                                        onDragLeave={e =>{setDropZoneActive(false); e.preventDefault()}}
+                                        onDrop={e=>{handleDrop(e)}}
+                                        className={'dropzone'+(dropZoneActive ? "active" :"")}>
+                                        Drop Zone                                                                                                                                                           
+                                    </div>
+
                                 </Box>
                             </Fade>
-                          
-                        </Modal>
+
+                        </Modal>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
                     </CardActions>
                 </React.Fragment></Card>
             </Box>
